9b9f5e3d18437dfa4185cd24c9d4c762a41f58b0
nmyers93 <neil.myers93@gmail.com>
2026-01-17 13:24:39 -0600

    Initial commit: Basic React scaffold with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>🍴 Fork n' Friends</h1>
+        <p>Decide where to eat with your friends</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
